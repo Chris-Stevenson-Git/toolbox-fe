c45dd5c3d313196c69e928f8f39364ee21c04f90
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2021-01-25 15:20:15 +1100

    Added starting bones of the app page

---
+++ src/components/netflix/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+class Landing extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>Netflix Page</h2>
+      </div>
+    )//return
+  }//render
+}//class Landing
+
+export default Landing
